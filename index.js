658629fc694f6b07b99b3849512550b33eb91c11
SynergixeNG <10416482+synergixe@users.noreply.github.com>
2019-06-24 19:27:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,74 @@
 /*!
- * @mrolaolu/helpers v0.0.2
+ * @mrolaolu/helpers v0.0.7
  * https://github.com/whizkydee/helpers
  * (c) 2019-present Olaolu Olawuyi
  * Released under the MIT License.
  */
 
-import uniq from './uniq'
-import toPx from './toPx'
-import sleep from './sleep'
-import rgbToHex from './rgbToHex'
-import debounce from './debounce'
-import countries from './countries'
-import emitEvent from './emitEvent'
-import dotAccess from './dotAccess'
-import resetScroll from './resetScroll'
-import getEventPath from './getEventPath'
-import scrollToElem from './scrollToElem'
-import smoothScroll from './smoothScroll'
-import elementInView from './elementInView'
+import uniq from './src/uniq'
+import toPx from './src/toPx'
+import wait from './src/wait'
+import sleep from './src/sleep'
+import rgbToHex from './src/rgbToHex'
+import weekdays from './src/weekdays'
+import countries from './src/countries'
+import findIndex from './src/findIndex'
+import emitEvent from './src/emitEvent'
+import dotAccess from './src/dotAccess'
+import isValueNan from './isValueNan' 
+import monthNames from './src/monthNames'
 import getUrlFields from './getUrlFields'
-import getFormFields from './getFormFields'
-import objectIsEmpty from './objectIsEmpty'
-import toAmountString from './toAmountString'
 import strictTypeOf from './strictTypeOf'
-import countryDialCodes from './countryDialCodes'
-import arrayToListItems from './arrayToListItems'
+import resetScroll from './src/resetScroll'
+import matchesQuery from './src/matchesQuery'
+import scrollToElem from './src/scrollToElem'
+import getEventPath from './src/getEventPath'
+import smoothScroll from './src/smoothScroll'
+import toAmountString from './toAmountString'
+import elementInView from './src/elementInView'
+import getFormFields from './src/getFormFields'
+import objectIsEmpty from './src/objectIsEmpty'
+import shortWeekdays from './src/shortWeekdays'
+import getAspectRatio from './src/getAspectRatio'
+import getFullWeekday from './src/getFullWeekday'
+import shortMonthNames from './src/shortMonthNames'
+import getShortWeekday from './src/getShortWeekday'
+import countryDialCodes from './src/countryDialCodes'
+import arrayToListItems from './src/arrayToListItems'
+import getFullMonthName from './src/getFullMonthName'
+import getShortMonthName from './src/getShortMonthName'
 
 export {
   uniq,
   toPx,
+  wait,
   sleep,
   rgbToHex,
-  debounce,
+  weekdays,
   countries,
   emitEvent,
   dotAccess,
+  isValueNan,
+  findIndex,
+  monthNames,
   resetScroll,
   getEventPath,
   scrollToElem,
-  smoothScroll,
-  elementInView,
-  objectIsEmpty,
-  toAmountString,
   getUrlFields,
   strictTypeOf,
+  smoothScroll,
+  matchesQuery,
+  elementInView,
+  objectIsEmpty,
+  shortWeekdays,
   getFormFields,
+  toAmountString,
+  getAspectRatio,
+  getFullWeekday,
+  shortMonthNames,
+  getShortWeekday,
   arrayToListItems,
   countryDialCodes,
+  getFullMonthName,
+  getShortMonthName,
 }
